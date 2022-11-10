d64c84617fc1952b9fba2d6c1a66bfc6dc99274c
zhoujianting <zhoujianting@newrank.cn>
2022-11-10 16:12:48 +0800

    chore: 配置prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2, // 指定每个缩进级别的空格数
+  useTabs: false, // 用制表符而不是空格缩进行。
+  semi: true, // 在语句末尾打印分号。
+  jsxSingleQuote: false, // 在JSX中使用单引号而不是双引号。
+  trailingComma: 'es5', // 在多行逗号分隔的语法结构中，尽可能打印尾随逗号。(例如，单行数组永远不会有尾随逗号。)
+  singleQuote: true, // 单引号代替双引号
+  bracketSpacing: true, // 在对象字面量的括号之间打印空格。
+  jsxBracketSameLine: false, // 将多行JSX元素的>放在最后一行的末尾，而不是单独放在下一行(不适用于自关闭元素)。
+  endOfLine: 'lf', // 换行
+  printWidth: 120, // 行的长度
+};
