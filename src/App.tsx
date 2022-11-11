c57a6fdda102d3979420d1fb1249590506fc3e0e
zhoujianting <zhoujianting@newrank.cn>
2022-11-11 14:51:18 +0800

    chore: 测试 css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import './App.css';
+import app from './app.module.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    <div className={app.App}>
+      <header className={app.AppHader}>
+        <p>This is title</p>
+        <a className={app.AppLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
