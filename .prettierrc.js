eafae50c6d7b5c5deb3cb1e8bf2fd8bf2c5837e4
zhoujianting <zhoujianting@newrank.cn>
2022-11-11 09:54:51 +0800

    fix: test

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,6 +7,6 @@ module.exports = {
   singleQuote: true, // 单引号代替双引号
   bracketSpacing: true, // 在对象字面量的括号之间打印空格。
   jsxBracketSameLine: false, // 将多行JSX元素的>放在最后一行的末尾，而不是单独放在下一行(不适用于自关闭元素)。
-  endOfLine: 'lf', // 换行
+  endOfLine: 'lf', // 换行，主要windows和Linux换行会出现不同
   printWidth: 120, // 行的长度
 };
